d23342d2d457b678ff58a64c46992c01f2d98c21
Emil <emilnizamov9@gmail.com>
2021-12-25 13:02:42 +0300

    Before adding Repository

---
+++ Services/Interfaces/IRepositoryBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Task_Tracker_Proj.Services.Interfaces
+{
+    interface IRepositoryBase<T>
+    {
+        IQueryable<T> Get(Expression<Func<T, bool>> expression);
+        void Update(T entity);
+        void Create(T entity);
+        void Delete(T entity);
+    }
+}
